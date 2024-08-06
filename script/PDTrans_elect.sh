db7b3820868b8fd403fc2dcf8049faba233fd51e
TimeCristal <krasimir.trifonov@gmail.com>
2024-08-06 22:17:55 +0000

    msh

--- script/PDTrans_elect.sh
+++ script/PDTrans_elect.sh
@@ -3,3 +3,5 @@ python PDTrans/prepdata.py --data_folder PDTrans/data/elect --dataset elect
 python evaluate.py --dataset='elect' --model-name='output_elect' --restore-file='best'
 
 python PDTrans/prepare_data_exchange.py --data_folder PDTrans/data/exchange --dataset exchange
+
+python PDTrans/train.py --model-name output_exchange --data-folder PDTrans/data/exchange --dataset exchange --lr 0.0001 --num_epochs 100
