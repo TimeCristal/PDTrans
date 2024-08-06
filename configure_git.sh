e2897aa4f4f1dcf7209ef7f16b7e5ea333c4229d
TimeCristal <krasimir.trifonov@gmail.com>
2024-08-06 21:44:31 +0000

    up

---
+++ configure_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set Git user name and email
+git config --global user.name "TimeCristal"
+git config --global user.email "krasimir.trifonov@gmail.com"
+
+echo "Git user.name and user.email have been configured."
